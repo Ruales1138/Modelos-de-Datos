29f50c89ba1d6dcbb039c5ab18b9ff3a3d549b62
David <hruales1138@gmail.com>
2025-09-19 07:00:16 -0500

    commit

---
+++ ejercicio_consultas.sql
@@ -0,0 +1,5 @@
+-- 1. Listar el nombre y el tipo de todos los registros en la tabla movies.
+select name, kind from movies
+
+-- 2. Listar el nombre y fecha de nacimiento de las personas que ya hayan fallecido, 
+-- ordenadas por fecha de nacimiento descendentemente.
