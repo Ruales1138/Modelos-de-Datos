c044bcfdda825bd3fc9dda993bdc475661571be8
David <hruales1138@gmail.com>
2025-09-19 07:21:19 -0500

    commit

--- ejercicio_consultas.sql
+++ ejercicio_consultas.sql
@@ -3,3 +3,11 @@ select name, kind from movies
 
 -- 2. Listar el nombre y fecha de nacimiento de las personas que ya hayan fallecido, 
 -- ordenadas por fecha de nacimiento descendentemente.
+select name, birthday
+from people
+where deathday is not null
+order by birthday desc
+
+-- 3. Listar el nombre, fecha de nacimiento, fecha de fallecimiento y la edad de las personas.
+select name, birthday, deathday, (date_part('year', deathday) - date_part('year', birthday)) as edad
+from people
